341584110d7ec443952dbd7dff51fa4e39db3b51
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-27 09:50:38 +0100

    Iniziato bonus + correzioni varie

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <h1>La mia prima app con Vue CLI!</h1>
+    <h1>Jumbotron</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Title',
+  name: 'Hero',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h1 {
-  color: red;
-  text-decoration: underline;
-}
 </style>
