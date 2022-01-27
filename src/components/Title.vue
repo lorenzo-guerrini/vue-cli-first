ce58fffcd6d250df6f28e1c1b12e7e43034e70bd
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-27 08:45:57 +0100

    Esercizio completato

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  color: red;
+  text-decoration: underline;
+}
+</style>
